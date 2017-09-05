414f5c248851114061cc447a630ec2b994fadd9c
Andrew Brooks <andrewbrooksct@gmail.com>
2017-09-05 00:09:04 -0400

    removing include_package_data=True from setup.py: just testing

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
     package_data={
         'sleeping_giant_edgelist': ['postman_problems/examples/sleepingiant/edgelist_sleeping_giant.csv'],
     },
-    include_package_data=True,
     entry_points={
         'console_scripts': ['chinese_postman=postman_problems.chinese_postman:main']
     },

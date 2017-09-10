5bc930c305fe1cbdf19c7245dd60df357021a8e4
Andrew Brooks <andrewbrooksct@gmail.com>
2017-09-10 08:04:49 -0400

    wrestling w numpy build.  about to remove dependency

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
 import os
 from setuptools import setup
-
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
+from setuptools.command.build_ext import build_ext as _build_ext
 
 
 def read(fname):
+    """
+    Utility function to read the README file.
+    Used for the long_description.  It's nice, because now 1) we have a top level
+    README file and 2) it's easier to type in the README file than to put a raw
+    string in below ...
+    """
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+class build_ext(_build_ext):
+    """
+    Hack needed to build numpy properly
+    See: https://stackoverflow.com/questions/19919905/how-to-bootstrap-numpy-installation-in-setup-py
+    """
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
 
 setup(
     name='postman_problems',
@@ -35,6 +50,8 @@ setup(
         'console_scripts': ['chinese_postman=postman_problems.chinese_postman:main']
     },
     python_requires='>=3.5',
+    cmdclass={'build_ext':build_ext},
+    setup_requires='numpy',
     install_requires=[
         'pandas',
         'networkx',

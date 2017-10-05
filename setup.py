298dc8636411430b32c05b75d24538a5b4b2c6e5
Andrew Brooks <andrewbrooksct@gmail.com>
2017-10-05 00:02:37 -0400

    bumping release to 0.2 in setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='postman_problems',
-    version='0.1.1',
+    version='0.2',
     author='Andrew Brooks',
     author_email='andrewbrooks@gmail.com',
     description='Solutions to Postman graph optimization problems: Chinese and Rural Postman problems',

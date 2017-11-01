22ee1988ee96b843409f15d378128115b96b10c0
Andrew Brooks <andrewbrooksct@gmail.com>
2017-10-31 22:17:14 -0400

    adding logging

---
+++ postman_problems/__init__.py
@@ -0,0 +1,5 @@
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
--- postman_problems/solver.py
+++ postman_problems/solver.py
@@ -1,5 +1,5 @@
 import itertools
-
+import logging
 import networkx as nx
 
 from postman_problems.graph import read_edgelist, create_networkx_graph_from_edgelist, create_required_graph, \
@@ -7,7 +7,12 @@ from postman_problems.graph import read_edgelist, create_networkx_graph_from_edg
     add_augmenting_path_to_graph, create_eulerian_circuit
 
 
-def rpp(edgelist_filename, start_node=None, edge_weight='distance'):
+#logger = logging.getLogger(__name__)
+logger_rpp = logging.getLogger( '{0}.{1}'.format( __name__, 'rpp'))
+logger_rpp = logging.getLogger( '{0}.{1}'.format( __name__, 'rpp'))
+
+
+def rpp(edgelist_filename, start_node=None, edge_weight='distance', verbose=False):
     """
     Solving the RPP from beginning (load network data) to end (finding optimal route).  This optimization makes a
      relatively strong assumption: the starting graph must stay a connected graph when optional edges are removed.
@@ -17,6 +22,7 @@ def rpp(edgelist_filename, start_node=None, edge_weight='distance'):
         edgelist_filename (str): filename of edgelist.  See cpp.py for more details
         start_node (str): name of starting node.  See cpp.py for more details
         edge_weight (str): name edge attribute that indicates distance to minimize in CPP
+        verbose (boolean): log info messages?
 
     Returns:
         tuple(list[tuple(str, str, dict)], networkx.MultiGraph]:
@@ -27,30 +33,37 @@ def rpp(edgelist_filename, start_node=None, edge_weight='distance'):
         The original graph is returned as well.  This is needed for visualization
     """
 
+    logger_rpp.disabled = not verbose
+
+    logger_rpp.info('read edgelist')
     el = read_edgelist(edgelist_filename, keep_optional=True)
+
+    logger_rpp.info('create full and required graph')
     g_full = create_networkx_graph_from_edgelist(el)
     g_req = create_required_graph(g_full)
     assert_graph_is_connected(g_req)
 
-    # get augmenting path for odd nodes
+    logger_rpp.info('getting odd node pairs')
     odd_nodes = get_odd_nodes(g_req)
     odd_node_pairs = list(itertools.combinations(odd_nodes, 2))
-    odd_node_pairs_shortest_paths = get_shortest_paths_distances(g_full, odd_node_pairs, edge_weight)
-    g_odd_complete = create_complete_graph(odd_node_pairs_shortest_paths, flip_weights=True)
 
-    # best solution using blossom algorithm
+    logger_rpp.info('get shortest paths between odd nodes')
+    odd_node_pairs_shortest_paths = get_shortest_paths_distances(g_full, odd_node_pairs, edge_weight)
+
+    logger_rpp.info('Find min weight matching using blossom algorithm')
+    g_odd_complete = create_complete_graph(odd_node_pairs_shortest_paths, flip_weights=True)
     odd_matching = dedupe_matching(nx.algorithms.max_weight_matching(g_odd_complete, True))
 
-    # add the min weight matching edges to g
+    logger_rpp.info('add the min weight matching edges to g')
     g_aug = add_augmenting_path_to_graph(g_req, odd_matching)
 
-    # get eulerian circuit route.
+    logger_rpp.info('get eulerian circuit route')
     circuit = list(create_eulerian_circuit(g_aug, g_full, start_node))
 
     return circuit, g_full
 
 
-def cpp(edgelist_filename, start_node=None, edge_weight='distance'):
+def cpp(edgelist_filename, start_node=None, edge_weight='distance', verbose=False):
     """
     Solving the CPP from beginning (load network data) to end (finding optimal route).
     Can be run from command line with arguments from cpp.py, or from an interactive Python session (ex jupyter notebook)
@@ -59,6 +72,7 @@ def cpp(edgelist_filename, start_node=None, edge_weight='distance'):
         edgelist_filename (str): filename of edgelist.  See cpp.py for more details
         start_node (str): name of starting node.  See cpp.py for more details
         edge_weight (str): name edge attribute that indicates distance to minimize in CPP
+        verbose (boolean): log info messages?
 
     Returns:
         tuple(list[tuple(str, str, dict)], networkx.MultiGraph]:
@@ -68,22 +82,25 @@ def cpp(edgelist_filename, start_node=None, edge_weight='distance'):
           The third element is the dict of edge attributes for that edge.
         The original graph is returned as well.  This is needed for visualization
     """
+    logger_cpp.disabled = not verbose
+
+    logger_cpp.info('read edgelist and create base graph')
     el = read_edgelist(edgelist_filename, keep_optional=False)
     g = create_networkx_graph_from_edgelist(el)
 
-    # get augmenting path for odd nodes
+    logger_cpp.info('get augmenting path for odd nodes')
     odd_nodes = get_odd_nodes(g)
     odd_node_pairs = list(itertools.combinations(odd_nodes, 2))
     odd_node_pairs_shortest_paths = get_shortest_paths_distances(g, odd_node_pairs, edge_weight)
     g_odd_complete = create_complete_graph(odd_node_pairs_shortest_paths, flip_weights=True)
 
-    # best solution using blossom algorithm
+    logger_cpp.info('Find min weight matching using blossom algorithm')
     odd_matching = dedupe_matching(nx.algorithms.max_weight_matching(g_odd_complete, True))
 
-    # add the min weight matching edges to g
+    logger_cpp.info('add the min weight matching edges to g')
     g_aug = add_augmenting_path_to_graph(g, odd_matching)
 
-    # get eulerian circuit route.
+    logger_cpp.info('get eulerian circuit route')
     circuit = list(create_eulerian_circuit(g_aug, g, start_node))
 
     return circuit, g

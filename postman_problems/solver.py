fe5d4bc554b6fa1e98c1eb3599002e88ca0dd18c
jackwackus <jackwackus@gmail.com>
2020-10-21 13:14:51 -0700

    Add files via upload

--- postman_problems/solver.py
+++ postman_problems/solver.py
@@ -1,105 +1,49 @@
-import itertools
-import logging
-import networkx as nx
+#	File Created: 10/15/2020
 
-from postman_problems.graph import read_edgelist, create_networkx_graph_from_edgelist, create_required_graph, \
-    assert_graph_is_connected, get_odd_nodes, get_shortest_paths_distances, create_complete_graph, dedupe_matching, \
-    add_augmenting_path_to_graph, create_eulerian_circuit
+"""
+modified version of solver by brooksandrew
+"""
 
+import postman_problems.solver_lib as sl
+from initialize_lib import turn_weight_function_distance, create_turn_weight_edge_attr
+from postman_problems.graph import read_edgelist, create_networkx_graph_from_edgelist, create_required_graph, assert_graph_is_strongly_connected, create_eulerian_circuit
 
-logger_rpp = logging.getLogger('{0}.{1}'.format(__name__, 'rpp'))
-logger_cpp = logging.getLogger('{0}.{1}'.format(__name__, 'cpp'))
-
-
-def rpp(edgelist_filename, start_node=None, edge_weight='distance', verbose=False):
+def rpp(edgelist_filename, complete_g, start_node=None, edge_weight='distance', turn_weight_coefficient=1):
     """
     Solving the RPP from beginning (load network data) to end (finding optimal route).  This optimization makes a
      relatively strong assumption: the starting graph must stay a connected graph when optional edges are removed.
     If this is not so, an assertion is raised.  This class of RPP generalizes to the CPP strategy.
-
     Args:
-        edgelist_filename (str): filename of edgelist.  See cpp.py for more details
-        start_node (str): name of starting node.  See cpp.py for more details
+        edgelist_filename (str): filename of edgelist.  
+        start_node (str): name of starting node.  
         edge_weight (str): name edge attribute that indicates distance to minimize in CPP
-        verbose (boolean): log info messages?
-
+        turn_weight_coefficient (float): turn weight coefficient used to add turn_weight attributes to g_full
     Returns:
-        tuple(list[tuple(str, str, dict)], networkx.MultiGraph]:
+        list[tuple(str, str, dict)]:
         Each tuple is a direction (from one node to another) from the CPP solution route.
           The first element is the starting ("from") node.
           The second element is the end ("to") node.
           The third element is the dict of edge attributes for that edge.
-        The original graph is returned as well.  This is needed for visualization
     """
 
-    logger_rpp.disabled = not verbose
+    el = read_edgelist(edgelist_filename)
 
-    logger_rpp.info('read edgelist')
-    el = read_edgelist(edgelist_filename, keep_optional=True)
-
-    logger_rpp.info('create full and required graph')
     g_full = create_networkx_graph_from_edgelist(el)
+    
+    g_full, pos = sl.create_pos_and_add_to_graph(g_full, complete_g)
+    
+    g_full = create_turn_weight_edge_attr(g_full, length_weight='distance', normalization_coefficient=turn_weight_coefficient)
+
     g_req = create_required_graph(g_full)
-    assert_graph_is_connected(g_req)
+    
+    sl.visualize_g_req(g_req, pos)
 
-    logger_rpp.info('getting odd node pairs')
-    odd_nodes = get_odd_nodes(g_req)
-    odd_node_pairs = list(itertools.combinations(odd_nodes, 2))
+    assert_graph_is_strongly_connected(g_req)
 
-    logger_rpp.info('get shortest paths between odd nodes')
-    odd_node_pairs_shortest_paths = get_shortest_paths_distances(g_full, odd_node_pairs, edge_weight)
+    g_aug = sl.make_graph_eulerian(g_req, g_full)
 
-    logger_rpp.info('Find min weight matching using blossom algorithm')
-    g_odd_complete = create_complete_graph(odd_node_pairs_shortest_paths, flip_weights=True)
-    odd_matching = dedupe_matching(nx.algorithms.max_weight_matching(g_odd_complete, True))
+    sl.is_graph_eulerian(g_aug)
 
-    logger_rpp.info('add the min weight matching edges to g')
-    g_aug = add_augmenting_path_to_graph(g_req, odd_matching)
+    circuit = list(create_eulerian_circuit(g_aug, g_full, str(start_node), edge_weight_name=turn_weight_function_distance))
 
-    logger_rpp.info('get eulerian circuit route')
-    circuit = list(create_eulerian_circuit(g_aug, g_full, start_node))
-
-    return circuit, g_full
-
-
-def cpp(edgelist_filename, start_node=None, edge_weight='distance', verbose=False):
-    """
-    Solving the CPP from beginning (load network data) to end (finding optimal route).
-    Can be run from command line with arguments from cpp.py, or from an interactive Python session (ex jupyter notebook)
-
-    Args:
-        edgelist_filename (str): filename of edgelist.  See cpp.py for more details
-        start_node (str): name of starting node.  See cpp.py for more details
-        edge_weight (str): name edge attribute that indicates distance to minimize in CPP
-        verbose (boolean): log info messages?
-
-    Returns:
-        tuple(list[tuple(str, str, dict)], networkx.MultiGraph]:
-        Each tuple is a direction (from one node to another) from the CPP solution route.
-          The first element is the starting ("from") node.
-          The second element is the end ("to") node.
-          The third element is the dict of edge attributes for that edge.
-        The original graph is returned as well.  This is needed for visualization
-    """
-    logger_cpp.disabled = not verbose
-
-    logger_cpp.info('read edgelist and create base graph')
-    el = read_edgelist(edgelist_filename, keep_optional=False)
-    g = create_networkx_graph_from_edgelist(el)
-
-    logger_cpp.info('get augmenting path for odd nodes')
-    odd_nodes = get_odd_nodes(g)
-    odd_node_pairs = list(itertools.combinations(odd_nodes, 2))
-    odd_node_pairs_shortest_paths = get_shortest_paths_distances(g, odd_node_pairs, edge_weight)
-    g_odd_complete = create_complete_graph(odd_node_pairs_shortest_paths, flip_weights=True)
-
-    logger_cpp.info('Find min weight matching using blossom algorithm')
-    odd_matching = dedupe_matching(nx.algorithms.max_weight_matching(g_odd_complete, True))
-
-    logger_cpp.info('add the min weight matching edges to g')
-    g_aug = add_augmenting_path_to_graph(g, odd_matching)
-
-    logger_cpp.info('get eulerian circuit route')
-    circuit = list(create_eulerian_circuit(g_aug, g, start_node))
-
-    return circuit, g
+    return circuit
